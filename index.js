e2a7d1011fd9380da4cdc5cf9bd96b81267a6f2b
volodymyr.vatamaniuk.ext <volodymyr.vatamaniuk.ext@bestsecret.com>
2022-02-23 13:09:24 +0100

    #the code has been improved.

--- index.js
+++ index.js
@@ -1,13 +1,10 @@
-
-
-  const refs = {
+const refs = {
     timer: document.querySelector("#timer-1"),
     days: document.querySelector('[data-value="days"]'),
     hours: document.querySelector('[data-value="hours"]'),
     mins: document.querySelector('[data-value="mins"]'),
     secs: document.querySelector('[data-value="secs"]'),
 }
-
 
 class Timer {
     constructor({ selector, targetDate }) {
@@ -25,31 +22,43 @@ class Timer {
     }
 
     start() {
-        setInterval(() => {
-            const deltaTime = this.targetDate - Date.now();
-            this.updateClockface(this.getTimeComponents(deltaTime));
+        setInterval(() => {           
+            this.updateClockface(
+                new TimeValue(this.targetDate).Value()
+            );
         }, 1000);
     }
-
     
     updateClockface({ days, hours, mins, secs }) {
-    this.refs.daysTimer.innerHTML = days;
-    this.refs.hoursTimer.innerHTML = hours;
-    this.refs.minutesTimer.innerHTML = mins;
-    this.refs.secondsTimer.innerHTML = secs;
-}
-    
-    getTimeComponents(time) {
-        const days = pad(Math.floor(time / (1000 * 60 * 60 * 24)));
-        const hours = pad(Math.floor((time % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60)));
-        const mins = pad(Math.floor((time % (1000 * 60 * 60)) / (1000 * 60)));
-        const secs = pad(Math.floor((time % (1000 * 60)) / 1000));
-
-        return { days, hours, mins, secs };
+        this.refs.daysTimer.innerHTML = days;
+        this.refs.hoursTimer.innerHTML = hours;
+        this.refs.minutesTimer.innerHTML = mins;
+        this.refs.secondsTimer.innerHTML = secs;
     }
 }
-    function pad(value) {
-        return String(value).padStart(2, '0');
+
+class TimeValue {
+    constructor(value) {
+        if (value === undefined || value === null) {
+           console.log('Parameter is null or undefined.');    
+        }  
+      this.value = value;
+  }        
+  
+ Value() {
+      let time = this.value - Date.now();
+
+      let days = pad(Math.floor(time / (1000 * 60 * 60 * 24)));
+      let hours = pad(Math.floor((time % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60)));
+      let mins = pad(Math.floor((time % (1000 * 60 * 60)) / (1000 * 60)));
+      let secs = pad(Math.floor((time % (1000 * 60)) / 1000));
+
+      return { days, hours, mins, secs };
+  }
+}
+
+function pad(value) {
+    return String(value).padStart(2, '0');
 }
 const CountDownTimer = new Timer({
     selector: '#timer-1',
